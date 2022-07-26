06db8bffaa816b74e9a50d0e381a5b1d614f885c
Samuel Amity <sam.amity@gmail.com>
2022-07-26 10:40:53 -0400

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+    form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    addTodo(e.target.newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function addTodo (todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteButton)
+  btn.textContent = 'COMPLETE!!' 
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteButton (e) {
+  e.target.parentNode.remove()
+}
